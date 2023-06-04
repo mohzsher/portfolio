1f774ebf054b5292662a433df4c0f3eca82fcd8c
Sher Mohammad <shermohammad@microsoft.com>
2023-06-05 00:16:22 +0530

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,42 +1,17 @@
-import { Stack,Checkbox  } from '@fluentui/react';
 import { Text } from '@fluentui/react/lib/Text';
 import Image from 'next/image';
 import home from "../app/images/home.jpg";
 
-
-
-// Used to add spacing between example checkboxes
-const stackTokens = { childrenGap: 10 };
-
-export const CheckboxBasicExample: React.FunctionComponent = () => {
-  // These checkboxes are uncontrolled because they don't set the `checked` prop.
-  return (
-    <Stack tokens={stackTokens}>
-      <Checkbox label="Unchecked checkbox (uncontrolled)" onChange={_onChange} />
-
-      <Checkbox label="Checked checkbox (uncontrolled)" defaultChecked onChange={_onChange} />
-
-      <Checkbox label="Disabled checkbox" disabled />
-
-      <Checkbox label="Disabled checked checkbox" disabled defaultChecked />
-    </Stack>
-  );
-};
-
-function _onChange(ev?: React.FormEvent<HTMLElement | HTMLInputElement>, isChecked?: boolean) {
-  console.log(`The option has been changed to ${isChecked}.`);
-}
-
 export default function Home() {
   return (
-    <Stack>
-     <Stack horizontalAlign='center'>
-        <Text variant='mega'>Sher Mohammad</Text>
-        <CheckboxBasicExample/>
-    </Stack>
-    <Stack>
-      <Image src={home} height={500} alt='hello'/>
-    </Stack>
-    </Stack>
+    <div className="flex-col content-center">
+      <div className="flex-none h-14">
+        <h1>Sher Mohammad</h1>
+      </div>
+      <div className="flex-initial">
+        <Image src={home} alt='sher' className='to-100%' />
+      </div>
+    </div>
+
   )
 }
--- pages/me/about.tsx
+++ pages/me/about.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
-  return (
-    <div className="container">
-        <h1>ABout Me</h1>
-    </div>)
-    }
+    return (
+        <div className="container">
+            <h1>ABout Me</h1>
+        </div>)
+}
